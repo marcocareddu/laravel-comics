fe1513b15f0c9d543b3cad32335ca6462ab10ee6
marcocareddu <saldumaccu@tuta.io>
2023-08-03 17:10:14 +0200

    Added assets bluemenu, footermenu

---
+++ config/blueMenu.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    [
+        "text" => "DIGITAL COMICS",
+        "picture" => "buy-comics-digital-comics.png"
+    ],
+    [
+        "text" => "DC MERCHANDISE",
+        "picture" => "buy-comics-merchandise.png"
+    ],
+    [
+        "text" => "SUBSCRIPTION",
+        "picture" => "buy-comics-subscriptions.png"
+    ],
+    [
+        "text" => "COMICS SHOP LOCATOR",
+        "picture" => "buy-comics-shop-locator.png"
+    ],
+    [
+        "text" => "DIGITAL COMICS",
+        "picture" => "buy-dc-power-visa.svg"
+    ]
+];
