90d1395b5f33292bc0cd2e6261ed532e2a4ac757
marcocareddu <saldumaccu@tuta.io>
2023-08-03 15:41:04 +0200

    INC Header - Added header inc

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="container">
+
+
+        <!-- Logo -->
+        <div class="logo">
+            <img class="img-fluid" src="" alt="Logo DC Comics">
+        </div>
+
+        <!-- Navbar -->
+        <nav>
+            <ul>
+
+                {{-- !TODO Foreache HERE --}}
+                <li><a></a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
