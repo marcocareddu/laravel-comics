fa13e2c00cf036a166793214f9c2d69110158ea1
marcocareddu <saldumaccu@tuta.io>
2023-08-03 16:23:39 +0200

    PHP Home - Add base content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,21 @@
 @extends('layouts.index')
+
+@section('main-content')
+    <div class="main-content">
+
+        <!-- Jumbotron -->
+        <div class="jumbotron"></div>
+
+        <!-- Main content -->
+        <section class="container">
+            <div class="row">
+
+                {{-- !TODO Card HERE --}}
+
+            </div>
+            <div class="btn-container">
+                <button>LOAD MORE</button>
+            </div>
+        </section>
+    </div>
+@endsection
